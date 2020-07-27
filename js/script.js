d6c6eb78813424812a1ffc49fed93f421a5205b9
Kezgan <lautaromcasella@gmail.com>
2020-07-27 00:09:33 -0300

    En progreso

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-var icon = document.querySelector("#icon-sky")
-var sky = document.querySelector('#current-sky')
+var icon_sky = document.querySelector("#icon-sky")
+var current_sky = document.querySelector('#current-sky')
 var temp = document.querySelector('#temp')
-var max = document.querySelector('#temp-max')
+var maxmin = document.querySelector('#temp-max-min')
 var min = document.querySelector('#temp-min')
 var now = new Date()
 var fecha = document.querySelector('#fecha')
+
+var icon_hum = document.querySelector("#icon-hum")
 
 // https://api.openweathermap.org/data/2.5/weather?q=northampton&units=metric&appid=6ec0c3105628563c8314d5586f05b7ac
 
@@ -34,12 +36,15 @@ fetch("https://api.openweathermap.org/data/2.5/onecall?lat=-34.5910185&lon=-58.6
 
 function mostrarResultados(clima) {
   console.log(clima)
+
   fecha.innerText = fechas(now)
-  icon.src = `http://openweathermap.org/img/wn/${clima.current.weather[0].icon}@2x.png`
-  /*sky.innerText = `${clima.current.weather[0].main}`*/
+
+  icon_sky.src = `http://openweathermap.org/img/wn/${clima.current.weather[0].icon}@2x.png`
+  current_sky.innerText = `${clima.current.weather[0].main}`
   temp.innerHTML = `${Math.round(clima.current.temp)}<span class="grados">°c</span>`
-  max.innerHTML = `${Math.round(clima.daily[0].temp.max)}<span class="grados">°c</span>`
-  min.innerHTML = `${Math.round(clima.daily[0].temp.min)}<span class="grados">°c</span>`
+  maxmin.innerHTML = `${Math.round(clima.daily[0].temp.max)}<span class="grados">°c</span>` + " | " + `${Math.round(clima.daily[0].temp.min)}<span class="grados">°c</span>`
+
+  icon_hum.src = `images/hum.png`
 }
 
 function fechas(d) {
